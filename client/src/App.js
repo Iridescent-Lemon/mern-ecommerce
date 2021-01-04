b70d1ca47d9b03632daa97b5777a17c58f845bfe
Iridescent-Lemon <Lemon.arjhay0110@gmail.com>
2021-01-04 13:58:56 +0000

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
 	return (
-		<>
+		<Router>
 			<Header />
 			<main className="py-3">
 				<Container>
-					<h1>Welcome to Tinda</h1>
+					<Switch>
+					<Route exact path="/" component={HomeScreen} />
+					<Route path="/products/:id" children={<ProductScreen />}>
+					</Route>
+					</Switch>
 				</Container>
 			</main>
 			<Footer />
-		</>
+		</Router>
 	);
 };
 
