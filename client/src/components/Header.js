ab3385456c4dbb76099c36acc5793f69cd3f9e89
Iridescent-Lemon <Lemon.arjhay0110@gmail.com>
2021-01-04 13:52:18 +0000

    modify Header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { Container, Navbar, Nav, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
 	return (
 		<header>
 			<Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
 				<Container>
-					<Navbar.Brand href="/">Tinda Online</Navbar.Brand>
+					<LinkContainer to="/">
+						<Navbar.Brand>Tinda Online</Navbar.Brand>
+					</LinkContainer>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="ml-auto">
-							<Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i> My Cart</Nav.Link>
-							<Nav.Link href="/login"><i className="fas fa-user"></i> Sign In</Nav.Link>
+							<LinkContainer to="/cart">
+								<Nav.Link>
+									<i className="fas fa-shopping-cart"></i> My Cart
+								</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to="/login">
+								<Nav.Link>
+									<i className="fas fa-user"></i> Sign In
+								</Nav.Link>
+							</LinkContainer>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
