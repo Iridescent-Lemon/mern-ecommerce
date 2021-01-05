431b96899f5d2d1b2edb9e012361596995fb06f5
Iridescent-Lemon <Lemon.arjhay0110@gmail.com>
2021-01-05 14:56:49 +0000

    config schema

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -1,38 +1,74 @@
 import mongoose, { Schema, model } from 'mongoose';
 
+const reviewSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	rating: {
+		type: Number,
+		required: true,
+		default: 0
+	},
+	comment: {
+		type: String,
+		required: true,
+	}
+	
+}, {timestamps: true});
+
 const productSchema = new Schema(
 	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: 'User'
+		},
 		name: {
 			type: String,
+			required: true,
 		},
 		image: {
 			type: String,
+			required: true,
 		},
 		description: {
 			type: String,
-		},
+			required: true,
+		}
+		reviews: [reviewSchema],
 		brand: {
 			type: String,
+			required: true,
 		},
 		category: {
 			type: String,
+			required: true,
 		},
 		price: {
 			type: Number,
+			required: true,
+			default: 0,
 		},
 		countInStock: {
 			type: Number,
+			required: true,
+			default: 0,
 		},
 		rating: {
 			type: Number,
+			required: true,
+			default: 0
 		},
 		numReviews: {
 			type: Number,
+			required: true,
+			default: 0
 		},
 	},
 	{ timestamps: true }
 );
 
-const Products = new model('product', productSchema);
+const Product = model('product', productSchema);
 
-export default Products;
+export default Product;
